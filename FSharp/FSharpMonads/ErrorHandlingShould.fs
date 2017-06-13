945e4fd9dded40c3030dabdbb0a1a39e4d807585
ouarzy <epecoul@gmail.com>
2017-06-13 11:25:13 +0200

    Initial step monads talk

--- FSharp/FSharpMonads/ErrorHandlingShould.fs
+++ FSharp/FSharpMonads/ErrorHandlingShould.fs
@@ -4,10 +4,10 @@ open Xunit
 open Swensen.Unquote
 open ErrorHandling
 
-// [<Fact>]
-// let ``string to int is positive int to string``() = 
-//     test <@ stringToIntIsPositiveIntToString "5" = Success "5" @>
+[<Fact>]
+let ``string to int is positive int to string``() = 
+    test <@ stringToIntIsPositiveIntToString "5" = "5" @>
 
-// [<Fact>]
-// let ``string to int is positive int to string fail``() = 
-//     test <@ stringToIntIsPositiveIntToString "toto" = Success "5" @>
+[<Fact>]
+let ``string to int is positive int to string fail``() = 
+    test <@ stringToIntIsPositiveIntToString "-3" = ? @>
